c016179eb09b805b727565e04685cd4287302a59
Matthew <matthewjcrosby1@gmail.com>
2023-06-12 23:34:03 +0700

    [FEAT]: validateSchema middleware

--- src/schema/quote.schema.ts
+++ src/schema/quote.schema.ts
@@ -1,11 +1,13 @@
 import { z } from 'zod'
 
 const QuoteSchema = z.object({
-    name: z.string(),
-    email: z.string(),
-    capability: z.string(),
-    comments: z.string(),
-    newsletter: z.boolean(),
+    body: z.object({
+        name: z.string(),
+        email: z.string(),
+        capability: z.string(),
+        comments: z.string(),
+        newsletter: z.boolean(),
+    }),
 })
 
 export default QuoteSchema
