0fa981516e52489ef87b059471697732230fb525
Matthew <matthewjcrosby1@gmail.com>
2023-06-13 15:08:41 +0700

    [FEAT]: post quote service

---
+++ src/service/quote.service.ts
@@ -0,0 +1,11 @@
+import { PostQuoteInput } from '../schema/quote.schema'
+
+const postQuoteService = ({
+    capability,
+    comments,
+    email,
+    name,
+    newsletter,
+}: PostQuoteInput) => {}
+
+export { postQuoteService }
